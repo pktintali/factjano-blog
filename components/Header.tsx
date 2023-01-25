864d8fa8a88b115cf0253c7860f6cad88cb7117a
pktintali <4gpradeep@gmail.com>
2023-01-25 16:13:40 +0530

    basic header design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+
 function Header() {
-    return (<>Header</>);
+  return (
+    <>
+      <header className="bg-gray-800 text-white">
+        <div className="container mx-auto flex items-center justify-between py-4">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            className="h-8 ml-2"
+            width={31}
+            height={31}
+          />
+          <h1 className="text-xl font-medium">Blog Title</h1>
+          <nav>
+            <Link
+              href="#tech"
+              className="px-3 text-sm font-medium hover:underline"
+            >
+              Tech
+            </Link>
+            <Link
+              href="#social"
+              className="px-3 text-sm font-medium hover:underline"
+            >
+              Social Media
+            </Link>
+            <Link
+              href="#jobs"
+              className="px-3 text-sm font-medium hover:underline"
+            >
+              Jobs
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Header from "@/components/Header";
 import Head from "next/head";
-// import { Inter } from "@next/font/google";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header/>
+        <Header />
       </main>
     </>
   );
